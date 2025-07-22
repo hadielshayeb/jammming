a00705f8b87c452a361989950c9ec2d279e0a31f
hadielshayeb <hadielshayeb@gmail.com>
2025-07-22 00:59:32 -0400

    new components

---
+++ src/Components/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import PlayList from '../Playlist/Playlist';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ja<span className="highlight">mm</span>ing</h1>
+      <SearchBar />
+      <div className="App-playlist">
+        <SearchResults />
+        <PlayList />
+      </div>
+    </div>
+  );
+}
