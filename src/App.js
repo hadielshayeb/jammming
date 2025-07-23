fc97c9f1b876cfa0f12fb2a5798108a600f96031
hadielshayeb <hadielshayeb@gmail.com>
2025-07-23 00:44:56 -0400

    Newer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import SearchBar from './Components/SearchBar/SearchBar';
 import SearchResults from './Components/SearchResults/SearchResults';
 import Playlist from './Components/Playlist/Playlist';
 
 function App() {
-
-  const sampleTracks = [
+  // Mock search results
+  const [searchResults, setSearchResults] = useState([
     {
       id: 1,
       name: "Shape of You",
@@ -25,16 +25,36 @@ function App() {
       artist: "Dua Lipa",
       album: "Future Nostalgia"
     }
-  ];
-  
+  ]);
+
+  const [playlist, setPlaylist] = useState("My Playlist");
+  const [playlistTracks, setPlaylistTracks] = useState([
+    { 
+      id: 4, 
+      name: 'Save Your Tears', 
+      artist: 'The Weeknd', 
+      album: 'After Hours' 
+    },
+    { id: 5, 
+      name: 'Blinding Lights', 
+      artist: 'The Weeknd', 
+      album: 'After Hours' 
+    },
+    { id: 6, 
+      name: 'Levitating', 
+      artist: 'Dua Lipa', 
+      album: 'Future Nostalgia' 
+    }
+  ]);
+
   return (
     <div className="App">
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <SearchBar />
       <div className="App-playlist">
         {/* Pass track data down to SearchResults */}
-        <SearchResults tracks={sampleTracks} />
-        <Playlist />
+        <SearchResults tracks={searchResults} />
+        <Playlist playlistname={playlist} playlistTracks={playlistTracks} />
       </div>
     </div>
   );
