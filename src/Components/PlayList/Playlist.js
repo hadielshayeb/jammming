428e6a231d43c0325e88b520b440676cbf74cd9b
hadielshayeb <hadielshayeb@gmail.com>
2025-07-24 21:36:20 -0400

    saving to spotify

--- src/Components/PlayList/Playlist.js
+++ src/Components/PlayList/Playlist.js
@@ -2,18 +2,16 @@ import React from 'react';
 import './Playlist.css';
 import Tracklist from '../Tracklist/Tracklist';
 
-function Playlist() {
-  // Placeholder playlist tracks
-  const playlistTracks = [
-    { id: 3, name: 'Playlist Track 1', artist: 'Artist 3', album: 'Album 3' },
-    { id: 4, name: 'Playlist Track 2', artist: 'Artist 4', album: 'Album 4' },
-  ];
+function Playlist({ playlistName, playlistTracks, onRemove, onNameChange, onSave }) {
+  const handleNameChange = (e) => {
+    onNameChange(e.target.value);
+  };
 
   return (
     <div className="Playlist">
-      <input defaultValue="New Playlist" />
-      <Tracklist tracks={playlistTracks} isRemoval={true} />
-      <button className="Playlist-save">Save To Spotify</button>
+      <input defaultValue={playlistName} onChange={handleNameChange} />
+      <Tracklist tracks={playlistTracks} onRemove={onRemove} isRemoval={true} />
+      <button className="Playlist-save" onClick={onSave}>Save To Spotify</button>
     </div>
   );
 }
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './Track.css';
 
-function Track({ track, isRemoval }) {
+function Track({ track, isRemoval, onAdd, onRemove }) {
+  const addTrack = () => {
+    onAdd(track);
+  };
+
+  const removeTrack = () => {
+    onRemove(track);
+  };
+
   return (
     <div className="Track">
       <div className="Track-information">
         <h3>{track.name}</h3>
         <p>{track.artist} | {track.album}</p>
       </div>
-      <button className="Track-action">{isRemoval ? '-' : '+'}</button>
+      {isRemoval ? (
+        <button className="Track-action" onClick={removeTrack}>-</button>
+      ) : (
+        <button className="Track-action" onClick={addTrack}>+</button>
+      )}
     </div>
   );
 }
